26f0cac15624682e0e88af32e484148d212f25ea
Rami Lahtinen <rami.lahtinen@aalto.fi>
2025-03-12 15:15:55 +0200

    Added sqlalchemy to requirements and based sql model

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
 
 app = FastAPI()
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
