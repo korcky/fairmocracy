02141eba8b5e742629a1c35362ef950163002cc6
Sergei Zhirnov <korckykrou@gmail.com>
2025-03-19 11:10:57 +0200

    [feat] tweaks to backend structure (#12)
    * [feat] introduce `Makefile` and simplify dependencies declaration
    
    * [feat] python files formatting
    
    * [fix] last mline formatting

--- backend/models/party.py
+++ backend/models/party.py
@@ -1,10 +1,9 @@
-from typing import List, Optional
 from sqlmodel import SQLModel, Field, Relationship
-from .voter import Voter
+
 
 class Party(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     name: str = Field()
-    voters: List["Voter"] = Relationship(back_populates="party")
+    voters: list["Voter"] = Relationship(back_populates="party")
     game_id: int = Field(foreign_key="game.id")
     game: "Game" = Relationship(back_populates="parties")
