65843adbbfbc4f58e64d83d8e5cab43a9b185524
Sergei Zhirnov <korckykrou@gmail.com>
2025-03-19 11:26:02 +0200

    [feat] initial endpoints dummies (#14)
    * [feat] introduce `Makefile` and simplify dependencies declaration
    
    * [feat] python files formatting
    
    * [fix] last mline formatting
    
    * [feat] add routers for user and voting

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from fastapi.responses import Response
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session, select
@@ -8,6 +8,9 @@ from sqlmodel import SQLModel, Session, select
 from models import Game, VotingEvent
 
 app = FastAPI()
+user_router = APIRouter()
+game_router = APIRouter()
+common_router = APIRouter()
 
 sqlite_file_name = "database.sqlite3"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -21,15 +24,52 @@ def on_startup():
     SQLModel.metadata.create_all(engine)
 
 
-@app.get("/games/")
+@common_router.get("/games/")
 async def read_games() -> list[Game]:
     with Session(engine) as session:
         games = session.exec(select(Game)).all()
         return games
 
 
-@app.get("/games/{game_id}")
+@common_router.get("/games/{game_id}")
 async def read_game(game_id: int) -> Game:
     with Session(engine) as session:
         game = session.exec(select(Game).where(Game.id == game_id)).first()
         return game or Response(status_code=HTTPStatus.BAD_REQUEST)
+
+
+@user_router.get(
+    "/{user_id}",
+    tags=["user"],
+)
+async def get_user(user_id: str):
+    return Response(status_code=HTTPStatus.NO_CONTENT)
+
+
+@user_router.post(
+    "/login",
+    tags=["user"],
+)
+async def login(key: str | None = None):
+    return Response(status_code=HTTPStatus.NO_CONTENT)
+
+
+@game_router.get(
+    "/current_state",
+    tags=["voting"],
+)
+async def get_current_state():
+    return Response(status_code=HTTPStatus.NO_CONTENT)
+
+
+@game_router.post(
+    "/cast_vote",
+    tags=["voting"],
+)
+async def post_vote(user_id: str, vote_id: str, vote: str, extra: dict[str, str] | None = None):
+    return Response(status_code=HTTPStatus.NO_CONTENT)
+
+
+app.include_router(common_router)
+app.include_router(user_router, prefix="/v1/user")
+app.include_router(game_router, prefix="/v1/voting")
