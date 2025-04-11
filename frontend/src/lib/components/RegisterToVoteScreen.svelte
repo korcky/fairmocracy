6e49a3d6e259ca0fb63baf7d6b85fb3f536f394a
Joonatan Syrjänen <joonatan.syrjanen@aalto.fi>
2025-04-11 16:54:14 +0300

    Revert "[Misc] Env variable workaround for Render"
    This reverts commit bf4cd23412c93c1620c443c7a00e0396904ba580.

--- frontend/src/lib/components/RegisterToVoteScreen.svelte
+++ frontend/src/lib/components/RegisterToVoteScreen.svelte
@@ -1,31 +1,12 @@
 <script>
 	import { z } from 'zod';
 	import { setUserData, currentUser } from '$lib/stores/userData.svelte.js';
-	// import { PUBLIC_BACKEND_URL } from "$env/static/public";
-
-	// Temporary workaround for Render, can revert back later
-	import { promises as fs } from 'fs';
-
-	async function getBackendUrl() {
-		try {
-			const localSecret = await import('$env/static/public');
-			return localSecret.PUBLIC_BACKEND_URL;
-		} catch (error) {
-			console.error('Error reading .env, using Render secret.', error);
-			const renderSecret = await fs.readFile('/etc/secrets/PUBLIC_BACKEND_URL', 'utf8');
-			return renderSecret.trim();
-		}
-	}
-
-	(async () => {
-		const PUBLIC_BACKEND_URL = await getBackendUrl();
-		console.log('Backend URL:', PUBLIC_BACKEND_URL);
-	})();
-
+    import { PUBLIC_BACKEND_URL } from "$env/static/public";
+	
 	let { onRegistration } = $props();
 	let party = $state('');
 	let errors = $state({});
-	let { game, name, userId, affiliations, rounds } = $currentUser;
+	let {game, name, userId, affiliations, rounds } = $currentUser;
 	let partyOptions = $state([]);
 	let gameState = JSON.parse(game.state);
 
@@ -33,7 +14,7 @@
 		if (game) {
 			fetch(`${PUBLIC_BACKEND_URL}/parties/game/${game.id}`).then((res) => {
 				if (res.ok) {
-					res.json().then((respJson) => (partyOptions = respJson));
+					res.json().then(respJson => partyOptions = respJson);
 				} else {
 					errors = { api: ['Failed to fetch parties'] };
 				}
@@ -41,19 +22,15 @@
 			if (!rounds || rounds.length == 0) {
 				fetch(`${PUBLIC_BACKEND_URL}/rounds/${game.id}`).then((res) => {
 					if (res.ok) {
-						res
-							.json()
-							.then((respJson) =>
-								setUserData({ game, name, userId, affiliations, rounds: respJson })
-							);
+						res.json().then(respJson => setUserData({ game, name, userId, affiliations, rounds: respJson }));
 					} else {
 						errors = { api: ['Failed to fetch rounds'] };
 					}
 				});
 			}
 		}
-	});
-
+	})
+	
 	let formValidator = z.object({
 		party: z.string().nonempty('Select your party')
 	});
@@ -61,43 +38,35 @@
 	const register = () => {
 		errors = {};
 		try {
-			const validatedData = formValidator.parse({ party });
+			const validatedData = formValidator.parse({  party });
 			const { party: validParty } = validatedData;
 			fetch(`${PUBLIC_BACKEND_URL}/register_to_vote`, {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
 				},
-				body: JSON.stringify({
+				body: JSON.stringify({ 
 					party_id: parseInt(party),
 					round_id: rounds[gameState.current_round].id,
-					voter_id: userId
-				})
+					voter_id: userId })
 			}).then((res) => {
 				if (res.ok) {
-					setUserData({
-						name,
-						game,
-						userId,
-						affiliations: { ...affiliations, [gameState.current_round]: { party: validParty } }
-					});
+					setUserData({name,game,userId,affiliations: { ...affiliations, [gameState.current_round]: { party: validParty } }});
 					onRegistration();
 				} else {
 					errors = { name: ['Registration failed'] };
 				}
-			});
+			})
 		} catch (err) {
-			console.error(err);
+			console.error(err)
 			if (err instanceof z.ZodError) {
 				errors = err.flatten().fieldErrors;
 			}
 		}
-	};
+	}
 </script>
 
-<p class="p-4 text-center text-lg">
-	Round {gameState.current_round}: select the party you represent.
-</p>
+<p class="p-4 text-center text-lg">Round {gameState.current_round}: select the party you represent.</p>
 
 <div class="form-container">
 	<div class="fields">
@@ -109,11 +78,11 @@
 				onchange={(e) => (party = e.target.value)}
 				class="select variant-form-material"
 			>
-				<option value="" disabled selected>Select your party</option>
-				{#if game}
-					{#each partyOptions as p}
-						<option value={p.id}>{p.name}</option>
-					{/each}
+			<option value="" disabled selected>Select your party</option>
+			{#if game}
+				{#each partyOptions as p}
+					<option value={p.id}>{p.name}</option>
+				{/each}
 				{/if}
 			</select>
 			{#if errors.party}
