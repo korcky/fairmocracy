8d8f4903235fa9daa34b72ce2e24f30383e3cf11
Janne Karjalainen <janne.karjalainen@aalto.fi>
2025-04-16 10:12:41 +0300

    make ConnectionManager singleton

--- backend/api/sse_connection_manager.py
+++ backend/api/sse_connection_manager.py
@@ -12,9 +12,15 @@ class SSEConnectionManager:
     A generator for pushing game state updates to clients
     from https://stackoverflow.com/a/79423537
     """
-    def __init__(self):
+    _instance = None
+
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_, *args, **kwargs)
+        return class_._instance
         self.active_connections: list[asyncio.Queue] = []
 
+    def __init__(self):
         loop = asyncio.get_running_loop()
         loop.add_signal_handler(signal.SIGINT, self.close)
 
@@ -36,4 +42,4 @@ class SSEConnectionManager:
         for queue in self.active_connections:
             queue.shutdown()
 
-
+manager = SSEConnectionManager()
