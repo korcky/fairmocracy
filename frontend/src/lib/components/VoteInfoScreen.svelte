0a6c33f3f35b81eb6fe53d40260eb10ec2864cb9
Joonatan Syrjänen <joonatan.syrjanen@aalto.fi>
2025-05-03 17:05:54 +0300

    [Feat] Show the question in VoteInfoScreen

--- frontend/src/lib/components/VoteInfoScreen.svelte
+++ frontend/src/lib/components/VoteInfoScreen.svelte
@@ -1,14 +1,21 @@
 <script>
+	import { get } from 'svelte/store';
 	import { hasSeenInfo } from '$lib/stores/screenStore.svelte.js';
+	import { currentUser } from '$lib/stores/userData.svelte.js';
+	import { gameState } from '$lib/stores/gameData.svelte.js';
 
 	function proceed() {
 		hasSeenInfo.set(true);
 	}
+
+	let { current_voting_question: currentQuestion } = get(gameState);
 </script>
 
 <div class="info-container mt-8 flex w-full flex-col items-center justify-center space-y-4">
-	<p>Info on the law that is going to be voted on</p>
-	<button type="button" class="variant-filled btn bg-blue-500" onclick={proceed}
-		>Proceed to Vote</button
-	>
+	<p class="w-full max-w-xl px-4 text-left text-lg whitespace-pre-wrap">
+		{currentQuestion}
+	</p>
+	<button type="button" class="variant-filled btn bg-blue-500" onclick={proceed}>
+		Proceed to Vote
+	</button>
 </div>
