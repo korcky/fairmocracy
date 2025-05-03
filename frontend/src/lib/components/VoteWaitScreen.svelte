b47daf2754143b902ec51d86b2228289f7747ba4
Joonatan Syrjänen <joonatan.syrjanen@aalto.fi>
2025-05-03 18:01:58 +0300

    Change vote time from 10 to 60 seconds

--- frontend/src/lib/components/VoteWaitScreen.svelte
+++ frontend/src/lib/components/VoteWaitScreen.svelte
@@ -5,7 +5,7 @@
 
 <div class="countdown-container mt-8 flex w-full flex-col items-center justify-center space-y-10">
 	<Countdown
-		countdownTime="10"
+		countdownTime="60"
 		countdownMessage="Waiting for others to finish voting."
 		finishedMessage="Waiting for next round of voting."
 	/>
