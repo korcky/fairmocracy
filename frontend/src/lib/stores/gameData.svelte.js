1129faccc9295b9b799e367135f6dd27c883312c
Joonatan Syrjänen <joonatan.syrjanen@aalto.fi>
2025-05-03 22:20:03 +0300

    [Feat] Count rounds from 1 instead of using round_id

--- frontend/src/lib/stores/gameData.svelte.js
+++ frontend/src/lib/stores/gameData.svelte.js
@@ -11,7 +11,9 @@ export const gameState = writable({
 	current_round_id: null,
 	current_voting_event_id: null,
 	current_voting_question: null,
-	countdown_ends_at: null
+	countdown_ends_at: null,
+	frontend_round_n: null,
+	frontend_event_n: null
 });
 
 let _lastLoadedGameId = null;
@@ -46,10 +48,32 @@ export function initGameStateSSE() {
 				return;
 			}
 
-			gameState.update((prev) => ({
-				...prev, // keep fields not in SSE
-				...data // overwrite/add only the SSE payload
-			}));
+			gameState.update((prev) => {
+				// if no previous round, initialize to 0 so that first will be 1 in any case
+				let newRoundN = prev.frontend_round_n ?? 0;
+				// check for new round
+				if (data.current_round_id != null && data.current_round_id !== prev.current_round_id) {
+					// double check to start at 1, otherwise increase round by 1
+					newRoundN = prev.current_round_id == null ? 1 : newRoundN + 1;
+				}
+
+				// check if event changed
+				let newEventN = prev.frontend_event_n ?? 0;
+				if (
+					data.current_voting_event_id != null &&
+					data.current_voting_event_id !== prev.current_voting_event_id
+				) {
+					// same as with round: either 1 or increase by 1
+					newEventN = prev.current_voting_event_id == null ? 1 : newEventN + 1;
+				}
+
+				return {
+					...prev, // keep fields not in SSE
+					...data, // overwrite/add only the SSE payload
+					frontend_round_n: newRoundN,
+					frontend_event_n: newEventN
+				};
+			});
 			getExtraInfo(); // Get latest extra info for user and their party when we receive new SSE
 		} catch (e) {
 			console.error('Invalid SSE payload:', evt.data, e);
@@ -74,7 +98,9 @@ export function resetGame() {
 		current_round_id: null,
 		current_voting_event_id: null,
 		current_voting_question: null,
-		countdown_ends_at: null
+		countdown_ends_at: null,
+		frontend_round_n: null,
+		frontend_event_n: null
 	});
 
 	parties.set([]);
