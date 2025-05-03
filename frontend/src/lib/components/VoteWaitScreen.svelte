a1e9a89d7ce53ba0d1fde5bbd0f0874cbaeedf44
Joonatan Syrjänen <joonatan.syrjanen@aalto.fi>
2025-05-03 18:10:49 +0300

    [Cleanup] Remove old demo button

--- frontend/src/lib/components/VoteWaitScreen.svelte
+++ frontend/src/lib/components/VoteWaitScreen.svelte
@@ -1,6 +1,5 @@
 <script>
 	import Countdown from './Countdown.svelte';
-	let { onNewVote } = $props();
 </script>
 
 <div class="countdown-container mt-8 flex w-full flex-col items-center justify-center space-y-10">
@@ -9,7 +8,4 @@
 		countdownMessage="Waiting for others to finish voting."
 		finishedMessage="Waiting for next round of voting."
 	/>
-	<button type="button" class="variant-filled-warning btn" onclick={onNewVote}
-		>Demo: Force New Vote</button
-	>
 </div>
